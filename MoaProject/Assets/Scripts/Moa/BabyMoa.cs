453d4dc71a617925ef7d4a8b5c6e00d4182ddef7
KGNZ <karlgraves28@gmail.com>
2017-03-28 14:17:36 +1300

    MoaFood and BabyMoa script
    BabyMoa script used for hunger meter that decays over time and increases when the Baby moa is fed

---
+++ MoaProject/Assets/Scripts/Moa/BabyMoa.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyMoa : MonoBehaviour {
+
+    public float hungerMeter = 10.0f; //float to store hunnger of baby moa
+    public float fHungerDecay = 0.1f; //float to use for decaying hunger meter over time, public so it can be adjusted in unity
+    //public GameObject Moa;
+    
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        HungerDecay();
+	}
+
+    void HungerDecay() //decays hungerMeter over time
+    {
+        hungerMeter -= fHungerDecay * Time.deltaTime;
+        if(fHungerDecay <= 0.0f)
+        {
+            //gameover
+        }
+        if(hungerMeter > 10.0f)
+        {
+            hungerMeter = 10.0f;
+        }
+    }
+
+   public void HungerMeterIncrease() //used when baby moa recieves food, increases the hunger meter
+    {
+        hungerMeter++;        
+    }
+}
--- MoaProject/Assets/Scripts/Moa/MoaFood.cs
+++ MoaProject/Assets/Scripts/Moa/MoaFood.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class MoaFood : MonoBehaviour
 {
 
-    public float fStones = 0.0f;
-    public float fBerries = 0.0f;
+    public float fStones = 0.0f; //float to store stones, public for testing
+    public float fBerries = 0.0f; //float to store berries/food, public for testing
 
-    public float stoneDecay = 1.0f;
-
-    bool bHasStones = false;
-    bool bCanEat = false;
+    public float stoneDecay = 0.1f; //float to use for decaying stones over time, public so it can be edited in unity
+   
+    //bool bHasStones = false;
+    bool bCanEat; //used in CanEatBerries()
 
 
     // Use this for initialization
@@ -23,11 +23,11 @@ public class MoaFood : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CanEatBerries();
+        //CanEatBerries();
         StoneDecay();
     }
 
-    bool CanEatBerries()
+    bool CanEatBerries() //will return true if the moa has stones
     {
         if (fStones > 0)
         {
@@ -41,35 +41,39 @@ public class MoaFood : MonoBehaviour
         return bCanEat;
     }
 
-    void StoneDecay()
+    void StoneDecay() //decays stones eaten over time
     {
-        if (bHasStones == true && fStones > 0)
+        if (bCanEat == true && fStones > 0)
         {
             fStones -= stoneDecay * Time.deltaTime;
+            //print("losing stones");
             if(fStones < 0)
             {
                 fStones = 0;
-                bHasStones = false;
+                bCanEat = false;
             }
         }
 
     }
 
-    void OnTriggerStay(Collider other)
+    void OnTriggerStay(Collider other) //compares colleder with gameobject tag to do actions
     {
         if(other.gameObject.CompareTag("Stone") && Input.GetKeyDown(KeyCode.E))
         {
             other.gameObject.SetActive(false);
             fStones++;
-            bHasStones = true;       
+            //bHasStones = true;
+            print("ate stone");       
         }
 
         if(other.gameObject.CompareTag("Berry") && Input.GetKeyDown(KeyCode.E))
         {
-            if(bCanEat == true)
+            CanEatBerries();
+            if (bCanEat == true)
             {
                 other.gameObject.SetActive(false);
                 fBerries++;
+                print("ate berry");
             }
             else
             {
@@ -82,6 +86,8 @@ public class MoaFood : MonoBehaviour
             if(fBerries > 0)
             {
                 fBerries--;
+                other.gameObject.GetComponent<BabyMoa>().HungerMeterIncrease();
+                print("fed berry");              
             }
             else
             {
